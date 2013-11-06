f65aa0d66792b50a87059b5d9532477378effd1b
Alvaro Serrano <marran51@gmail.com>
2013-11-06 12:47:14 -0300

    Definicion de headers de entrada

--- saga.py
+++ saga.py
@@ -1,1 +1,17 @@
-saga.py
+def audio_local():
+	'''
+	Función que reproduce un archivo mp3
+	'''
+	pass
+
+def audio_analogo():
+	'''
+	Función que reproduce la entrada de audio analoga
+	'''
+	pass
+
+def audio_web():
+	'''
+	Función que reproduce un stream de audio desde internet
+	'''
+	pass
