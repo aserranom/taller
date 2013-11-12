c21b47b8363cc9e1bde28ce7ca1d636cc2c47d0c
Alvaro Serrano <marran51@gmail.com>
2013-11-12 21:34:50 +0000

    comentarios y rutina para detener la reproducción

--- saga.py
+++ saga.py
@@ -18,10 +18,17 @@ def audio_web():
 	'''
 	Función que reproduce un stream de audio desde internet
 	'''
+	# Asegurarse de tener cargado el módulo de sonido
 	os.system("sudo modprobe snd_bcm2835")
+	# Url de entrada
 	url = "http://65.60.34.34:8030"
 	user_url = raw_input("Indica una URL o apreta enter para escuchar una por default: ")
 	if user_url:
 		url = user_url
+	# Se agrega la Url a la lista de playlists de mpc
 	os.system("sudo mpc add " + url)
+	# Se toca la Url agregada
 	os.system("sudo mpc play")
+	# Se detiene la reproducción
+	raw_input("Presione Enter para continuar...")
+	os.system("sudo mpc stop")
