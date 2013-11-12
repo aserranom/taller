7ee9c487886d1fede9d6315abaa9e4670ba95a75
Alvaro Serrano <marran51@gmail.com>
2013-11-12 21:28:24 +0000

    encoding y logica del audio web

--- saga.py
+++ saga.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import os
+
 def audio_local():
 	'''
 	Función que reproduce un archivo mp3
@@ -14,4 +18,10 @@ def audio_web():
 	'''
 	Función que reproduce un stream de audio desde internet
 	'''
-	pass
+	os.system("sudo modprobe snd_bcm2835")
+	url = "http://65.60.34.34:8030"
+	user_url = raw_input("Indica una URL o apreta enter para escuchar una por default: ")
+	if user_url:
+		url = user_url
+	os.system("sudo mpc add " + url)
+	os.system("sudo mpc play")
