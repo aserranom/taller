93d655a56c052e18b7825277dbe82d748e48e0b6
amermelao <amermelao@gmail.com>
2013-12-18 20:03:48 -0300

    borraron comentario de funciones antiguas

--- saga.py
+++ saga.py
@@ -138,23 +138,6 @@ class audio_web(InputType):
 		os.remove('dump.wav')
 	
 		
-def audio_local():
-	'''
-	Función que reproduce un archivo mp3
-	'''
-	# Asegurarse de tener cargado el módulo de sonido
-	os.system("sudo modprobe snd_bcm2835")
-	# Borrar la lista anterior
-	os.system("sudo mpc clear")
-	# Agregar al playlist el archivo de audio
-	# los archivos deben estar antes en  la carpeta /var/lib/mpd/music
-	os.system("sudo mpc update")
-	os.system("sudo mpc add *.mp3")
-	# Se Reproduce el archivo
-	os.system("sudo mpc play")#sudo
-	# Se detiene la reproducción
-	raw_input("Presione Enter para continuar...")
-	os.system("sudo mpc stop")#sudo
 
 class audio_analogo(InputType):
 	'''
@@ -209,20 +192,4 @@ class audio_analogo(InputType):
 	        self.reproduciendo = False
 	        self.stream.close()
 	        self.p.terminate()
-'''
-def audio_web():
-	# Asegurarse de tener cargado el módulo de sonido
-	os.system("sudo modprobe snd_bcm2835")
-	# Url de entrada
-	url = "http://65.60.34.34:8030"
-	user_url = raw_input("Indica una URL o apreta enter para escuchar una por default: ")
-	if user_url:
-		url = user_url
-	# Se agrega la Url a la lista de playlists de mpc
-	os.system("sudo mpc add echo " + url)#sudo mpc add
-	# Se toca la Url agregada
-	os.system("sudo mpc play")#sudo
-	# Se detiene la reproducción
-	raw_input("Presione Enter para continuar...")
-	os.system("sudo mpc stop")#sudo
-'''
+
