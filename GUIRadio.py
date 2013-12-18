f576cdd7007714538b4f015e55e11def51a266cf
amermelao <amermelao@gmail.com>
2013-12-18 18:03:44 -0300

    falta agregar los objetos de entrada

--- GUIRadio.py
+++ GUIRadio.py
@@ -1,7 +1,23 @@
 from Tkinter import *
 from RadioInterface import *
-ventana = Tk()
-ventana.wm_title("SAGA 1 - Taller de Diseño")
-playList = PlayList()
-radioInterface = RadioInterface(ventana, playList)
-ventana.mainloop()
+import thread
+import audio_controler
+from saga import *
+    
+    
+    
+if __name__ == "__main__":
+    ventana = Tk()
+    ventana.wm_title("SAGA 1 - Taller de Diseno")
+    playList = PlayList()
+    #input_ = 
+    control = audio_controler.audio_controler( (Test1(1),Test1(2)) )
+    radioInterface = RadioInterface(ventana, playList,control)
+    thread.start_new(control.start, ())
+    print('lol')
+    ventana.mainloop()
+    
+    
+    #thread.start_new_thread(initInterface, ventana)
+    
+    
