41e437879df96766e35dadf33fedf9281b7022d9
amermelao <amermelao@gmail.com>
2013-12-18 17:20:04 -0300

    bla 2

--- audio_controler.py
+++ audio_controler.py
@@ -1,48 +1,70 @@
 import saga
 import thread
 import os
+from audio_controler import *
 
 
 
 class audio_controler:
 
-	def __init__(self,passwrd = "",input_):
+	def __init__(self,input_):
 		
 		self.funciones = []	
 		for inpt in input_:
 			self.funciones.append(inpt)
 		print self.funciones
+		self.reproducir = 0
+		self.isPause = False
 		#os.system("echo " + passwrd + " | sudo -S echo 'its on'")
 
+	def back(self):
+		self.funciones[self.reproducir].back()
+	
+	def play(self):
+		self.isPause = False
+		self.funciones[self.reproducir].play()
+	
+	def pause(self):
+		self.isPause = True
+		self.funciones[self.reproducir].pause()
+	
+	def stop(self):
+		self.funciones[self.reproducir].stop()
+	
+	def forward(self):
+		self.funciones[self.reproducir].next()
+	
+	
 	def start(self):
 		'''indica cual es la entrada con la que va a inicia la reproduccion'''
-		reproducir = 2
+		
 		cont = 0
 		begin_thread = True
 		'''Esto es para testar el codifo'''
-		print self.funciones[0].can_play(),self.funciones[1].can_play(),self.funciones[2].can_play()
+		#print self.funciones[0].can_play(),self.funciones[1].can_play(),self.funciones[2].can_play()
 		'''hasta aca'''
 		while 1 and True:	
 			
-
+			if self.isPause:
+				continue
 		
 			if begin_thread:
-				thread.start_new_thread( self.funciones[reproducir].play,())
+				thread.start_new_thread( self.funciones[self.reproducir].play,())
 				begin_thread = False
-			if reproducir > 0:
-				for i in range(reproducir):
+			if self.reproducir > 0:
+				for i in range(self.reproducir):
 					if self.funciones[i].can_play():
-						self.funciones[reproducir].stop()
-						reproducir = i
+						self.funciones[self.reproducir].stop()
+						self.reproducir = i
 						begin_thread = True
 		
 						break
-			if not self.funciones[reproducir].can_play():
-				self.funciones[reproducir].stop()# es para combiar la entrada si no esta reproduciendo
-				if reproducir < len(self.funciones)-1:
+			if not self.funciones[self.reproducir].can_play():
+				self.funciones[self.reproducir].stop()# es para combiar la entrada si no esta reproduciendo
+				if self.reproducir < len(self.funciones)-1:
 					begin_thread = True
-					reproducir += 1
-				elif len(self.funciones)-1 == reproducir:
+					self.reproducir += 1
+				elif len(self.funciones)-1 == self.reproducir:
 					raise Exception("Nothing to play")
 					
 				
